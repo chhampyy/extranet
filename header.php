07d8a1b04adc08aa76849914a51abf151f4a9866
kevin <kevin.champagnol@gmail.com>
2020-12-11 10:11:42 +0100

    fichier header servant à l'include

---
+++ header.php
@@ -0,0 +1,7 @@
+<header>
+	
+	<img src="css/images/logo_gbaf.png" alt="Logo GBAF Groupement Banque-Assurance Français" class="logo"/>
+	<div class="user_info"><img src="css/images/icon_user.png" alt="icone utilisation noir et blanc" class="user_icone" />Nom & Prénom</div>
+		
+	
+</header>
