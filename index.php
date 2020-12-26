e88f3d337a1c82ca730e94a6f7eaaa22bded9a0e
kevin <kevin.champagnol@gmail.com>
2020-12-27 00:11:00 +0100

    passage de la page index de html à php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Extranet</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+	
+    <body>
+		<?php include("header.php"); ?>
+		<hr/>
+		
+		<div class="container_presentation">
+			<div class="texte_presentation">
+			<h1>Présentation</h1>
+			<p>Le site GBAF est une plateforme regroupant plusieurs agence bancaire. Grâce au site, vous serez en mesure de voir directement les avis concernant chaques acteurs partenaires. Un gain de temps précieux dans notre travail.</p>
+			</div>
+		</div>
+		
+			<div class="illustration">
+			<img src="css/images/extranet.png" alt="illustration d'un extranet"/>
+			</div>
+		
+		<hr>
+		
+		<div class="container_description">		
+		<h2>Acteurs et partenaires</h2>
+		<p>Indigentia appareat amantur ad et sensu quoddam et illa est parentes quale natos indigentia moribus animadverti mihi similis quod cum cum et evidentius cuius etiam sit quam in quantum facile.</p>
+		</div>
+		
+		<div class="container_tableau">
+			<div class="tableau">
+				<div class="mini_logo"><img src="css/images/formation_co.png"/></div>
+					<div class="texte_position">
+						<h3>Nom Acteur</h3>
+						<p>Le Lorem Ipsum Lorem Ipsum. </p>
+						<div class="button"><input class="forme_button" type="button" value="Lire la suite"></div>
+					</div>
+					
+			</div>
+			
+		</div>
+		<hr/>
+		<?php include("footer.php"); ?>
+
+    </body>
+	
+</html>
