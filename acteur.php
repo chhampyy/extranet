6fd78e6aad4f9f504b01b175382a799de98a3c82
kevin <kevin.champagnol@gmail.com>
2021-01-20 00:36:32 +0100

    modification d'ajout de commentaire. Le champs textarea s'affiche desormais après avoir cliqué sur le bouton nouveau commentaire, et disparait une fois le commentaire posté

--- acteur.php
+++ acteur.php
@@ -9,10 +9,9 @@ if (!$session_connecte AND $loginPage == false) {
 function curdate() {
     return date('d-m-Y');
 }
-
+$formulaire_com = Null;
 $error = Null;
 $succes = Null;
-
 //Affichage des information de la page acteur selectionnée 
 $req_acteur = $bdd->prepare('SELECT id_acteur, acteur, description, logo FROM acteur WHERE id_acteur = ? ');
 $req_acteur->execute(array($_GET['acteur_id']));
@@ -23,21 +22,30 @@ $id_existe = (int) $_GET['acteur_id'];
 if(!empty($id_existe) AND $id_existe <= $donnees['id_acteur']) {
 //Si ID de l'acteur est présent dans l'URL et qu'il est un numerique et inférieur ou égale au nombre d'acteur dans la base alors ont affiche
 
-     if (!empty($_POST['new_commentaire'])) {
-		$commentaire = htmlspecialchars($_POST['commentaire']);
-	
-		if (!empty($commentaire)) {
+
+     if (isset($_POST['new_commentaire'])) {
+		
+		$formulaire_com = '<form action="" method="POST">
+									<textarea name="commentaire" placeholder="Ajouter un commentaire..." class="textarea" ></textarea><br>
+									<button type="submit" name="envoie_com" class="envoie_com">Envoyer</button>
+								</form>';
+}
+else{
+	if (!empty($_POST['commentaire'])) {
 			$succes = 'merci pour votre commentaire';
 			$rm_commentaire = $bdd->prepare('INSERT INTO post(id_user, id_acteur, date_add, post) VALUES(?, ?, curdate() , ?)');
 			$rm_commentaire->bindValue(1, $_SESSION['id_user'], PDO::PARAM_INT);
 			$rm_commentaire->bindValue(2, $donnees['id_acteur'], PDO::PARAM_INT);
-			$rm_commentaire->bindValue(3, $commentaire, PDO::PARAM_STR);
+			$rm_commentaire->bindValue(3, htmlspecialchars($_POST['commentaire']), PDO::PARAM_STR);
 			$rm_commentaire->execute();
 		}
-		else{
-		$error = 'Renseignez un commentaire dans le champs ci dessus';
-		}
-	}
+}
+
+	
+	
+
+
+	
 
 //Recupération du nombre de commentaire sur page acteur selectionné
 $req_nb_commentaire = $bdd->prepare('SELECT count(*) AS TotalCommentaire FROM post WHERE id_acteur = :id_acteur');
@@ -105,7 +113,7 @@ else{
     <body>
 		<hr/>
 			<div class="container_acteur">
-				<div class="image_acteur"><img src="<?= $donnees['logo'] ?>" /></div>
+				<div class="image_acteur"><img src="<?= $donnees['logo'] ?>" class="acteur_logo"/></div>
 					<div class="texte_acteur">
 						<h2><?= $donnees['acteur'] ?></h2>
 							<p><a href="#">Lien acteur</a></p>
@@ -113,25 +121,31 @@ else{
 					</div> 
 			</div>
 			<div class="container_commentaire">
-					<div class="nombre_commentaire">
-						<p><?= $nombre_commentaire['TotalCommentaire'] ?> Commentaire<?= ($nombre_commentaire['TotalCommentaire'] > 1 ) ? 's' : ''; ?></p>
-					</div>	
-						<div>
-							<form action="" method="POST" name="nouveau_commentaire_utilisateur">
-								<div class="new_commentaire">
-									<input class="bouton_commentaire" type="submit" value="Nouveau commentaire" name="new_commentaire" />
-									<textarea name="commentaire" placeholder="Ajouter un commentaire..." class="textarea" ></textarea>
-									<p><?= $succes ?> <?= $error ?></p>
-								</div>
-							</form>
+						
+						<div class="fonction_commentaire">
+							<div class="nombre_commentaire">
+								<span class="nb_com"><?= $nombre_commentaire['TotalCommentaire'] ?> Commentaire<?= ($nombre_commentaire['TotalCommentaire'] > 1 ) ? 's' : ''; ?></span>
+							</div>
+							<div class="new_com">
+								<form action="" method="POST"/>
+									<button class="bouton_commentaire" type="submit" name="new_commentaire">Nouveau commentaire</button>
+								</form>
+							</div>
 								<div class="like_dislike">
 									<div class="images_pouces">
 										<form action="" method="POST" name="vote_user">
-											<button type="submit" name="likes" value="2"><img src="css/images/like.png" alt="bouton like" /> <?= $nombre_like['TotalAime'] ?> </button>
-											<button type="submit" name="dislikes" value="1"><img src="css/images/dislike.png" alt="bouton dislike" /> <?= $nombre_dislike['TotalDislike'] ?> </button>
+											<button type="submit" name="likes" value="2" <?php if (!empty($_POST['likes'])): ?> class="like_active" <?php endif ?>><img src="css/images/like.png" alt="bouton like" class="like" /> <?= $nombre_like['TotalAime'] ?> </button>
+											<button type="submit" name="dislikes" value="1" <?php if (!empty($_POST['dislikes'])): ?> class="dislike_active" <?php endif ?>><img src="css/images/dislike.png" alt="bouton dislike" class="dislike" /> <?= $nombre_dislike['TotalDislike'] ?> </button>
 										</form> 
 									</div>
 								</div>
+							</div>
+							<div class="champs_commentaire">
+								<form action="" method="POST">
+									<?= $formulaire_com ?>
+									<p><?= $succes ?> <?= $error ?></p>
+								</form>
+							</div>
 								<?php while ($affiche = $req_commentaire->fetch()):?>
 									<div class="commentaire">
 										<p><strong>Prenom:</strong> <?= $affiche['prenom'] ?><br/>
@@ -139,7 +153,7 @@ else{
 										<strong>Commentaire:</strong> <?= $affiche['post'] ?></p>
 									</div>
 								<?php endwhile ?>
-						</div>
+						
 			</div>
 		<hr/>
 		<?php include("footer.php"); ?>
