5f511fe2806a3e5f9b6614d95cf48b01a51d4278
kevin <kevin.champagnol@gmail.com>
2021-01-20 00:32:45 +0100

    Correction bug session start

--- header.php
+++ header.php
@@ -8,10 +8,21 @@ if (isset($_SESSION['nom']) AND isset($_SESSION['prenom'])){
 	$session_connecte = ''.$_SESSION['nom'].' '.$_SESSION['prenom'].'';
 }
 ?>
-<header>
-	
-	<a href="index.php"><img src="css/images/logo_gbaf.png" alt="Logo GBAF Groupement Banque-Assurance Français" class="logo"/></a>
-	<div class="user_info"><a href="parametre.php" title="Gestion de compte" class="link"><?= $session_connecte ?></a><a href="logout.php"><?= isset($session_connecte) ? '<img src="css/images/user_logout.png" alt="se déconnecter" title="Se déconnecter"/>' : ''; ?> </a></div>
-		
-	
-</header>
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<header>
+		<div class="header"> 
+			<a href="index.php"><img src="css/images/logo_gbaf.png" alt="Logo GBAF Groupement Banque-Assurance Français" class="logo"/></a>
+			
+		<div class="user_info">
+			<a href="parametre.php" title="Gestion de compte" class="link"><?= $session_connecte ?></a>
+			<a href="logout.php"><?= isset($session_connecte) ? '<img src="css/images/user_logout.png" alt="se déconnecter" title="Se déconnecter"/>' : ''; ?> </a>
+		</div>
+		</div>
+	</header>
+</body>
+</html>
